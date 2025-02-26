d8a2593f2374dbe2a3286a800db11110006971b8
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-26 15:38:01 +0530

    ilandImage is created for laptop screen but mobile and tablet responsive is due

--- src/Components/IlandImages/IlandImages.jsx
+++ src/Components/IlandImages/IlandImages.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const IlandImages = () => {
-      return (
-            <div className='border-2 py-space240 border-red-500'>
+  return (
+    <div className="  ">
+      <div className="flex pt-space240 py-space120 mb-[3rem] justify-between">
 
-               <div>hii</div>   
-            </div>
-      );
+      <div className="flex justify-center items-center gap-space15 border-2 border-blue-500">
+        <img src="/public/ilandImages/left/IMG_8584.svg" alt="img" />
+        <img src="/public/ilandImages/left/Yogapith,_Mayapur 2.svg" alt="img" />
+        <img src="/public/ilandImages/left/IMG_8557.svg" alt="img" />
+        <img src="/public/ilandImages/left/NMP1-111 1.svg" alt="img" />
+      </div>
+      {/* text  */}
+      <div>
+        <h2 className="text-heading2 font-primaryLight w-72 text-center border-2 border-red-500">Islands of Navadvip</h2>
+
+      </div>
+      <div className="flex gap-space15 justify-center items-center  border-2 border-blue-500">
+            <img src="/public/ilandImages/right/1986-01-16 1.svg" alt="" />
+            <img src="/public/ilandImages/right/IMG_8425.svg" alt="" />
+            <img src="/public/ilandImages/right/IMG_8487.svg" alt="" />
+            <img src="/public/ilandImages/right/NMP1-111 1 (1).svg" alt="" />
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default IlandImages;
