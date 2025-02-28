5e79de9a0ae10b729a409946c246ed210adbac58
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-03-01 00:24:12 +0530

    day one responsiveness is created

--- src/Components/ParikramaDays/Day-1/DayOne.jsx
+++ src/Components/ParikramaDays/Day-1/DayOne.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const DayOne = () => {
   return (
-    <div className="pt-space300 pb-space300 border-2 border-white  bg-primaryBlack">
-      <div className="w-11/12 mx-auto border-2 border-red-900">
-        <div className="ml-40">
-          <p className="text-success font-primaryLight mb-space120 text-center text-heading4">
+    <div className=" w-full md:py-space300 py-space120 border-2 border-white  bg-primaryBlack">
+      <div className="w-11/12 mx-auto">
+        <div className="lg:ml-40 mb-spacelg">
+          <p className="text-success font-primaryLight mb-space60 md:mb-space120 text-center text-heading4">
             Parikama
           </p>
-          <h1 className="font-primaryLight text-heading text-success text-center">
+          <h1 className="font-primaryLight text-heading3  md:text-heading text-success text-center">
             Day One
           </h1>
         </div>
-        <div className="my-space120 flex justify-center ml-44">
-          <img src="/public/Day Images/dayOne.svg" alt="" className="" />
+        <div className="my-space60 md:my-space120 flex justify-center lg:ml-44">
+          <img src="/public/Day Images/dayOne.svg" alt="" className="w-[242px] md:w-auto" />
         </div>
-        <div className="mb-space120 border-2 w-fit mx-auto border-red-600 text-success font-primaryLight text-heading2  text-left">
+        <div className="mb-space30 md:mb-space120  w-fit lg:mx-auto text-success font-primaryLight text-heading3 md:text-heading2  text-left">
           <h3 className=" tracking-wider ">
-            Antardvip <br />& Simantadvi
+            Antardvip <br />& Simantadvip
           </h3>
         </div>
         {/* next continer  */}
-        <div className=" border-2 border-red-400 pl-space300">
-            <div className="w-[500px] border-2 border-red-400 mx-auto">
+        <div className="mt-spacelg mb:mt-0  lg:pl-space300">
+            <div className="md:w-[500px]  lg:mx-auto">
           <p className="mb-spacelg text-primaryWhite font-primaryLight text-heading4 tracking-wider">
             Visiting Places<span className="ml-spacelg">( 13</span> <span className="">)</span>
           </p>
-          <h6 className="font-primaryLight text-heading4 tracking-wider text-success">
+          <h6 className="font-primaryLight md:text-heading4 tracking-wider text-success">
             Sri Chaitanya Gaudiya Math → Nandan Acharya Bhavan → Yogapith →
             Shrivas Angan → Advaita Bhavan → Gadadhar Angan → Murari Gupta Angan
             → Prithu Kunda → Sri Chaitanya Math → Shardanga → Sridhar Angan →
