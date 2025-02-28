385b153c7d3e241864a2cd08e8f7cf4fed550c85
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-28 19:34:03 +0530

    map is added and text added too

--- src/Components/ParikramaDays/Day-1/DayOne.jsx
+++ src/Components/ParikramaDays/Day-1/DayOne.jsx
@@ -8,7 +8,10 @@ const DayOne = () => {
                         <p className='text-success font-primaryLight mb-space120 text-center text-heading4'>Parikama</p>
                         <h1 className='font-primaryLight text-heading text-success text-center'>Day One</h1>
                   </div>
-                  <div className='mt-space120 flex justify-center ml-44'><img src="/public/Day Images/dayOne.svg" alt="" className='' /></div>
+                  <div className='my-space120 flex justify-center ml-44'><img src="/public/Day Images/dayOne.svg" alt="" className='' /></div>
+                  <div className='border-2 w-fit mx-auto border-red-600 text-success font-primaryLight text-heading2  text-left'>
+                  <h3 className=' tracking-wider ml-28'>Antardvip <br />& Simantadvi</h3>
+                  </div>
                   </div>
             </div>
       );
