125b71a4d2795369895e48cc4e020132a1a426cb
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-03-06 20:34:21 +0530

    sticky part fixed

--- src/Components/NavadwipMeaning/NavadwipMeaning.jsx
+++ src/Components/NavadwipMeaning/NavadwipMeaning.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavadwipMeaning = () => {
       return (
-            <div className='w-11/12 md:w-full mix-blend-difference md:grid grid-cols-12  md:my-space240 bg-primaryBlack'>
+            <div className='w-11/12 md:w-full relative  md:grid grid-cols-12  md:my-space240 text-primaryBlack'>
                   <p className='text-center tracking-wider  md:col-start-3 md:col-span-8  font-primaryLight text-mobileTextRegular  md:text-textRegular  mx-auto'>Navadvip means “Nine (nava) Islands (dvip)” surrounded by the sacred branches of the Ganges. It unfolds like a lotus. At its heart lies the birthplace of Sri Chaitanya, symbolizing self-surrender. From surrender, devotion blossoms—each petal of this lotus representing hearing, chanting, remembering, serving, worshipping, praying, servitorship, and friendship. As we journey through these sacred places, their stories reveal to us the essence of true devotion.</p>
             </div>
       );
