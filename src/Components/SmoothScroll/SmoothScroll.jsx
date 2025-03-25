4702778600c53bf51ce24e0570dacb13999cdd23
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-03-25 13:57:47 +0530

    bug fixed for saffari

--- src/Components/SmoothScroll/SmoothScroll.jsx
+++ src/Components/SmoothScroll/SmoothScroll.jsx
@@ -3,22 +3,26 @@ import { useLocation } from "react-router-dom";
 
 const SmoothScroll = () => {
   const location = useLocation();
+
   useEffect(() => {
     if (location.hash) {
       const targetId = location.hash.substring(1).replace(/\s+/g, "-");
       const element = document.getElementById(targetId);
 
       if (element) {
-        if (element) {
-          setTimeout(() => {
-            const isMobile = window.innerWidth < 768;
-            const yOffset = isMobile ? 10 : -80;
-            const y =
-              element.getBoundingClientRect().top + window.scrollY + yOffset;
-            window.scrollTo({ top: y, behavior: "auto" });
-       
-          }, 100);
-        }
+        setTimeout(() => {
+          const isMobile = window.innerWidth < 768;
+          const yOffset = isMobile ? 10 : -80;
+          const y =
+            element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+          // Safari Fix: scrollTo fallback
+          try {
+            window.scrollTo({ top: y, behavior: "instant" }); // "instant" better for Safari
+          } catch (e) {
+            element.scrollIntoView({ behavior: "auto", block: "center" });
+          }
+        }, 100);
       }
     }
   }, [location]);
