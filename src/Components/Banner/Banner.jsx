f5ffb8d5a5ab7af01b814b5642a9abfc420e2ec3
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-03-30 02:12:14 +0530

    banner work is under process

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 import NavadwipMeaning from "../NavadwipMeaning/NavadwipMeaning";
 import IlandImages from "../IlandImages/IlandImages";
 
-
 const Banner = () => {
-
-
-
   return (
     <section
       id="topContainer"
-      className=" md:pt-space120 border-2 border-red-600 flex flex-col justify-center items-center "
+      className=" md:pt-space120 flex flex-col justify-center items-center "
     >
       <svg
-  
-        className="mix-blend-difference  sticky z-20 place-content-center w-[50%] md:w-[30%] top-[50%] fill-success  -translate-y-1/2 block"
+        className="mix-blend-difference  sticky z-20  w-[50%] md:w-[30%] top-1/2 fill-success  -translate-y-1/2  border border-blue-600"
         viewBox="0 0 450 528"
         xmlns="http://www.w3.org/2000/svg"
       >
--- src/Components/IlandImages/IlandImages.jsx
+++ src/Components/IlandImages/IlandImages.jsx
@@ -1,7 +1,7 @@
 const IlandImages = () => {
   return (
-    <div className="border-2 border-blue-500 min-h-screen w-full place-items-center place-content-center">
-      <div className="  flex-col flex md:flex-row justify-center items-center mx-auto w-full   md:justify-between">
+    <div className="min-h-screen border-2 border-red-300 w-full mx-auto place-content-center md:place-items-center">
+      <div className="  flex-col flex md:flex-row justify-center items-center  w-full   md:justify-between">
         <div className="hidden lg:flex justify-center items-center gap-space15 ">
           <img src="/Images/Iland images/IMG_8584.svg" alt="img" />
           <img src="/Images/Iland images/Yogapith,_Mayapur 2.svg" alt="img" />
@@ -14,7 +14,7 @@ const IlandImages = () => {
           Islands of Navadvip
         </h2>
 
-        <div className=" hidden lg:flex gap-space15 justify-center items-center  ">
+        <div className=" hidden lg:flex gap-space15 justify-center items-center   ">
           <img src="/Images/Iland images/1986-01-16 1.svg" alt="" />
           <img src="/Images/Iland images/IMG_8425.svg" alt="" />
           <img src="/Images/Iland images/IMG_8487.svg" alt="" />
