10aabaf96dd8d19c914ae45dd6fe8782a2daa996
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-07 00:29:56 +0530

    land images are updated

--- src/Components/IlandImages/IlandImages.jsx
+++ src/Components/IlandImages/IlandImages.jsx
@@ -3,10 +3,10 @@ const IlandImages = () => {
     <div className="min-h-screen border-2 border-red-300 w-full mx-auto place-content-center md:place-items-center">
       <div className="  flex-col flex md:flex-row justify-center items-center  w-full   md:justify-between">
         <div className="hidden lg:flex justify-center items-center gap-space15 ">
-          <img src="/Images/Iland images/IMG_8584.svg" alt="img" />
-          <img src="/Images/Iland images/Yogapith,_Mayapur 2.svg" alt="img" />
-          <img src="/Images/Iland images/IMG_8557.svg" alt="img" />
-          <img src="/Images/Iland images/NMP1-111 1.svg" alt="img" />
+          <img src="/Images/Iland images/IMG_8584.webp" alt="img" />
+          <img src="/Images/Iland images/Yogapith,_Mayapur 2.webp" alt="img" />
+          <img src="/Images/Iland images/IMG_8557.webp" alt="img" />
+          <img src="/public/Images/Iland images/NMP1-111 1 (4).webp" alt="img" />
         </div>
         {/* text  */}
 
@@ -15,10 +15,10 @@ const IlandImages = () => {
         </h2>
 
         <div className=" hidden lg:flex gap-space15 justify-center items-center   ">
-          <img src="/Images/Iland images/1986-01-16 1.svg" alt="" />
-          <img src="/Images/Iland images/IMG_8425.svg" alt="" />
-          <img src="/Images/Iland images/IMG_8487.svg" alt="" />
-          <img src="/Images/Iland images/NMP1-111 1 (1).svg" alt="" />
+          <img src="/Images/Iland images/1986-01-16 1.webp" alt="" />
+          <img src="/Images/Iland images/IMG_8425.webp" alt="" />
+          <img src="/Images/Iland images/IMG_8487.webp" alt="" />
+          <img src="/Images/Iland images/NMP1-111 1 (1).webp" alt="" />
         </div>
       </div>
     </div>
