fd965506efa53885a34dad84c27d6fe228c5fb6e
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-07 15:13:25 +0530

    animation bug fixed

--- src/Hooks/usetextAnimation.jsx
+++ src/Hooks/usetextAnimation.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,87 +6,96 @@ import SplitType from "split-type";
 import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 const usetextAnimation = () => {
-      const containerRef = useRef();
-      useGSAP(
-        () => {
-          if (containerRef.current) {
-            const title = gsap.utils.toArray(".title");
-            title.forEach((el) => {
-              const words = new SplitType(el, { types: "chars" });
-              console.log(words.chars)
-              gsap.from(words.chars, {
-                opacity: 0,
-                y: 30,
-                duration: 0.6,
-                stagger: 0.02,
-                ease: "power4.out",
-                scrollTrigger: {
-                  trigger: el,
-                  start: "top 70%",
-                  end: "top 50%",
-                  // scrub: true,
-                  // markers: true,
-                },
-              });
-            });
-          }
-        },
-        { scope: containerRef }
-      );
-      // line animation
-      useGSAP(
-        () => {
-          if (containerRef.current) {
-            const element = gsap.utils.toArray(".animateLines");
-            element.forEach((el, i) => {
-              const split = new SplitType(el, { types: "lines" });
-              console.log(split.lines)
-              gsap.from(split.lines, {
-                opacity: 0,
-                y: 30,
-                duration: 0.04,
-                stagger: 0.1,
-                ease: "power4.out",
-                scrollTrigger: {
-                  trigger: el,
-                  start: "top 70%",
-                  end: "top 50%",
-                  // scrub: true,
-                  id: `line-${i}`,
-                  // markers: true,
-                },
-              });
-            });
-          }
-        },
-        { scope: containerRef }
-      );
-      useGSAP(()=>{
-        if(containerRef.current){
-          const paragrpah = gsap.utils.toArray(".paragraph")
-          console.log(paragrpah)
-          paragrpah.forEach((el,i)=>{
-            gsap.from(el,{
-              opacity: 0,
-                y: 30,
-                duration: 0.08,
-                stagger: 0.2,
-                ease: "power4.out",
-                scrollTrigger: {
-                  trigger: el,
-                  start: "top 70%",
-                  end: "top 50%",
-                  // scrub: true,
-                  id: `line-${i}`,
-                  // markers: true,
-                },
-            })
-          })
-        }
-    
-      },{scope: containerRef})
-    
-      return containerRef;
+  const containerRef = useRef();
+  useGSAP(
+    () => {
+      if (containerRef.current) {
+        const title = gsap.utils.toArray(".title");
+        title.forEach((el) => {
+          const words = new SplitType(el, { types: "chars" });
+          console.log(words.chars);
+          gsap.from(words.chars, {
+            opacity: 0,
+            y: 30,
+            duration: 0.6,
+            stagger: 0.02,
+            ease: "power4.out",
+            scrollTrigger: {
+              trigger: el,
+              start: "top 70%",
+              end: "top 50%",
+              // scrub: true,
+              // markers: true,
+            },
+          });
+        });
+      }
+    },
+    { scope: containerRef }
+  );
+  // line animation
+  useGSAP(
+    () => {
+      if (containerRef.current) {
+        const element = gsap.utils.toArray(".animateLines");
+        element.forEach((el, i) => {
+          const split = new SplitType(el, { types: "lines" });
+          console.log(split.lines);
+          gsap.from(split.lines, {
+            opacity: 0,
+            y: 30,
+            duration: 0.04,
+            stagger: 0.1,
+            ease: "power4.out",
+            scrollTrigger: {
+              trigger: el,
+              start: "top 70%",
+              end: "top 50%",
+              // scrub: true,
+              id: `line-${i}`,
+              // markers: true,
+            },
+          });
+        });
+      }
+    },
+    { scope: containerRef }
+  );
+  useGSAP(
+    () => {
+      if (containerRef.current) {
+        const paragrpah = gsap.utils.toArray(".paragraph");
+        console.log(paragrpah);
+        paragrpah.forEach((el, i) => {
+          gsap.from(el, {
+            opacity: 0,
+            y: 30,
+            duration: 0.08,
+            stagger: 0.2,
+            ease: "power4.out",
+            scrollTrigger: {
+              trigger: el,
+              start: "top 70%",
+              end: "top 50%",
+              // scrub: true,
+              id: `line-${i}`,
+              // markers: true,
+            },
+          });
+        });
+      }
+    },
+    { scope: containerRef }
+  );
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 500); // wating for dom load 
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return containerRef;
 };
 
 export default usetextAnimation;
